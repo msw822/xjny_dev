7c5a07340f86201b5c788ea9f318972e7465c0db
m15129 <mashaowei@h3c.com>
2018-02-09 20:34:01 +0800

    新疆农业大学sql 初始开发版本

---
+++ xjny_一卡通主题.sql
@@ -0,0 +1,95 @@
+--  DESCRIPTION: ods->fdm 一卡通主题---- 
+--     OPTIONS: ---
+-- REQUIREMENTS: ---
+--        BUGS: ---
+--        NOTES: ---
+--       AUTHOR: li.xiwang@h3c.com
+--     COMPANY: h3c.bigdata
+--      VERSION: 1.0
+--     CREATED: 2018/02/08
+--    REVIEWER:
+--    REVISION:
+
+SET mapred.output.compress = true;
+SET hive.exec.compress.output = true;
+SET mapred.output.compression.codec = org.apache.hadoop.io.compress.SnappyCodec;
+SET mapred.output.compression.type = block;
+
+--  DESCRIPTION: ods->fdm 一卡通账户信息表(fdm_ykt_xx)
+INSERT OVERWRITE TABLE fdm.fdm_ykt_xx PARTITION ( dt = '2018-01-01' ) 
+SELECT
+kh,
+NULL,
+NULL,
+NULL,
+NULL,
+xm,
+NULL,
+sfrzh,
+NULL,
+NULL,
+NULL,
+zhye,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+sfrzh,
+NULL,
+xh,
+NULL,
+zcrq,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+kpzt 
+FROM
+ods.ods_usr_gxsj_t_ykt_kh;
+
+--  DESCRIPTION: ods->fdm 一卡通商户信息表(fdm_ykt_shxx)
+INSERT OVERWRITE TABLE fdm.fdm_ykt_shxx PARTITION ( dt = '2018-01-01' ) 
+SELECT
+shid,
+shmc,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL 
+FROM
+ods.ods_usr_gxsj_t_ykt_shxx;
+
+--  DESCRIPTION: ods->fdm 一卡通交易信息表(fdm_ykt_jy)			
+INSERT OVERWRITE TABLE fdm.fdm_ykt_jy PARTITION ( dt = '2018-01-01' ) 
+SELECT
+a.kh,
+NULL,
+b.xh,
+NULL,
+A.XFJE,
+NULL,
+A.XFRQ,
+a.xfrqsj,
+C.SHMC,
+c.shid,
+c.shmc,
+a.xfsbbh,
+NULL,
+a.xfye 
+FROM
+( SELECT kh,xfje,xfrq,xfrqsj,xfsbbh,xfye,xfdwid FROM ods.ods_usr_gxsj_t_ykt_xfjl WHERE dt = '2018-01-01' ) A
+LEFT JOIN ( SELECT kh,xh FROM ods.ods_usr_gxsj_t_ykt_KH  ) B ON A.KH = B.kh
+LEFT JOIN ( SELECT shmc,shid,xfsbid,xfdwid FROM ods.ods_usr_gxsj_t_ykt_xfsbbh ) c ON (
+CAST ( CAST ( C.xfsbid AS int ) AS string )) = a.xfsbbh 
+AND (
+CAST ( CAST ( c.xfdwid AS int ) AS string )) = a.xfdwid;
---
+++ xjny_课程主题.sql
@@ -0,0 +1,118 @@
+ --  DESCRIPTION: ods->fdm 课程主题---- 
+--     OPTIONS: ---
+-- REQUIREMENTS: ---
+--        BUGS: ---
+--        NOTES: ---
+--       AUTHOR: li.xiwang@h3c.com
+--     COMPANY: h3c.bigdata
+--      VERSION: 1.0
+--     CREATED: 2018/02/08
+--    REVIEWER:
+--    REVISION:
+
+SET mapred.output.compress = true;
+SET hive.exec.compress.output = true;
+SET mapred.output.compression.codec = org.apache.hadoop.io.compress.SnappyCodec;
+SET mapred.output.compression.type = block;
+
+--  DESCRIPTION: ods->fdm 课程基本信息表(fdm_kc_jbxx)
+INSERT OVERWRITE TABLE fdm.fdm_kc_jbxx PARTITION ( dt = '2999-12-31' ) 
+SELECT
+A.KCDM,
+A.KCMC,
+A.KCYWMC,
+A.XF,
+A.ZHXS,
+A.XS,
+NULL,
+B.SYXS,
+A.KCJJ,
+NULL,
+NULL,
+NULL,
+A.KCKSDWH,
+NULL,
+NULL
+from 
+(select KCDM,
+KCMC,
+KCYWMC,
+XF,
+ZHXS,
+XS,
+KCJJ,
+KCKSDWH FROM ODS.ods_usr_gxsj_t_bzks_kc )A
+LEFT JOIN ( SELECT SYXS,KCDM FROM ods.ods_usr_gxsj_T_BZKS_JXJH  ) B ON A.KCDM = B.KCDM; 
+
+--  DESCRIPTION: ods->fdm 成绩基本信息表(fdm_kc_cjjbxx)							
+INSERT OVERWRITE TABLE fdm.fdm_kc_cjjbxx PARTITION ( dt ='2999-12-31' ) 
+select
+A.xn,
+A.xqdm,
+NULL,
+A.xh,
+A.KCDM,
+NULL,
+NULL,
+NULL,
+NULL,
+A.BFZKSCJ,
+A.DJZKSCJ,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+B.JLKCYXDM,
+NULL,
+A.xf,
+NULL,
+NULL,
+A.jd,
+NULL,
+NULL,
+NULL,
+NULL
+from 
+(select XN,
+XQDM,
+XH,
+KCDM,
+BFZKSCJ,
+DJZKSCJ,
+XF,
+JD
+FROM ODS.ods_usr_gxsj_t_bzks_kscj )A
+LEFT JOIN ( SELECT JLKCYXDM,KCDM FROM ods.ods_usr_gxsj_T_BZKS_KC) B 
+ON A.KCDM = B.KCDM; 
+
+--  DESCRIPTION: ods->fdm 排课信息表(edu_fdm_kc_pk)
+
+
+
+--  DESCRIPTION: ods->fdm 选课信息表(fdm_kc_xsxkxx)						
+INSERT OVERWRITE TABLE fdm.fdm_kc_xsxkxx PARTITION ( dt = '2999-12-31' ) 
+select 
+kch,
+null,
+jxbh,
+b.KKXN,
+c.zdlszgh,
+xh,
+null,
+null,
+b.KKXQ,
+null
+from 
+( 
+SELECT kch,jxbh,xh,wid from ods.ods_usr_gxsj_t_bzks_xsxk  ) A
+LEFT JOIN ( SELECT wid,kkxn,KKXQ FROM ods.ods_usr_gxsj_T_JX_KCAP  ) B ON A.wid = B.wid
+LEFT JOIN ( SELECT wid,zdlszgh FROM ods.ods_usr_gxsj_T_BZKS_PKXX_BST  ) c ON A.wid = c.wid
