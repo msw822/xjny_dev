4704f6bbb5f1d3a83dbd0999d66459794f87aa60
m15129 <mashaowei@h3c.com>
2018-02-11 11:30:57 +0800

    名称

--- python/fdm_ts_jyjl_incr.py
+++ python/fdm_ts_jyjl_incr.py
@@ -1,8 +1,7 @@
 # -*-coding:utf-8-*-
-
+import datetime
 import os
 import sys
-import datetime
 
 #获取外部日期参数，如果传参则data_day_str为传递的参数，如果未传参data_day_str为昨天的日期
 if(len(sys.argv)>1):
@@ -23,80 +22,47 @@ else:
 # 运行的时候将分区字段替换成  dt = '"""+data_day_str+"""' #
 ###########################################################
 
-
 sql = """
 set mapred.output.compress=true; 
 set hive.exec.compress.output=true; 
 set mapred.output.compression.codec=org.apache.hadoop.io.compress.SnappyCodec; 
 set mapred.output.compression.type=block;
 
-
-
---  DESCRIPTION: ods->fdm 读者信息表(fdm_ts_dz)
-INSERT OVERWRITE TABLE fdm.fdm_ts_dzxx PARTITION (dt = '2999-12-31') 
+--  DESCRIPTION: ods->fdm 图书借阅记录表(fdm_ts_jyjl)
+INSERT OVERWRITE TABLE fdm.fdm_ts_jyjl PARTITION (dt = '2017-02-02') 
 SELECT
-sfrzh,
-dztm,
-sfrzh,
-zjhm,
-NULL,
-NULL,
-NULL,
-bzrq,
-NULL,
-NULL,
-NULL,
-qkje,
-NULL,
-NULL,
-ljcc,
-NULL,
-NULL,
-NULL,
-NULL
+  a.tstm,
+  b.tm,
+  a.sfrzh,
+  a.sfrzh,
+  a.rq,
+  NULL,
+  c.dzxm,
+  a.rq,
+  NULL,
+  NULL,
+  NULL
 FROM
-ods.ods_usr_gxsj_t_ts_dz;
+  (
+    SELECT
+      sfrzh,
+      tstm,
+      czlx,
+      rq
+    FROM
+      ods.ods_usr_gxsj_T_TS_JHSHU WHERE dt='2017-02-02' AND
+    czlx = 'WJ'
+  ) a
+LEFT JOIN (SELECT tstm, tm FROM ods.ods_usr_gxsj_T_TS_TSXX) b ON a.TSTM = b.TSTM
+LEFT JOIN ods.ods_usr_gxsj_t_ts_dz c ON a.sfrzh = c.sfrzh
 
 
 
---  DESCRIPTION: ods->fdm 图书信息表(fdm_ts_tsxx)
-INSERT OVERWRITE TABLE fdm.fdm_ts_tsxx PARTITION (dt = '2999-12-31') 
-SELECT
-dlh,
-tstm,
-tm,
-NULL,
-NULL,
-NULL,
-NULL,
-NULL,
-NULL,
-jg,
-NULL,
-NULL,
-ztflh,
-NULL,
-NULL,
-NULL,
-NULL,
-NULL,
-NULL,
-NULL,
-NULL,
-cbz,
-NULL,
-cbd,
-NULL,
-NULL,
-NULL,
-NULL
-FROM
-ods.ods_usr_gxsj_t_ts_tsxx;
+
 
 """
-
 #hiveShell = 'hive -e "' + sql + '"'
-
-hiveShell = """su hdfs -c \"hive -e \\\"""" + sql + """\\\"\""""print(hiveShell)
+hiveShell = """su hdfs -c \"hive -e \\\"""" + sql + """\\\"\""""
+print(hiveShell)
 os.system(hiveShell)
 print('finish--')
