6093b4e7835a4a4632995c34fe03d313990f381a
m15129 <mashaowei@h3c.com>
2018-02-12 10:42:23 +0800

    删除多余）

--- python/fdm_ykt_jy_incr.py
+++ python/fdm_ykt_jy_incr.py
@@ -46,7 +46,7 @@ NULL,
 a.xfye 
 FROM
 ( SELECT kh,xfje,xfrq,xfrqsj,xfsbbh,xfye,xfdwid FROM ods.ods_usr_gxsj_t_ykt_xfjl  WHERE dt = '"""+data_day_str+"""' ) A
-LEFT JOIN ( SELECT kh,xh FROM ods.ods_usr_gxsj_t_ykt_KH WHERE dt = '2999-12-31' ) ) B ON A.KH = B.kh
+LEFT JOIN ( SELECT kh,xh FROM ods.ods_usr_gxsj_t_ykt_KH WHERE dt = '2999-12-31'  ) B ON A.KH = B.kh
 LEFT JOIN ( SELECT shmc,shid,xfsbid,xfdwid FROM ods.ods_usr_gxsj_t_ykt_xfsbbh WHERE dt = '2999-12-31') c ON (
 CAST ( CAST ( C.xfsbid AS int ) AS string )) = a.xfsbbh 
 AND (
