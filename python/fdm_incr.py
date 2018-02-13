6818c8e652431442a515baeeba9743c073e0bfe7
m15129 <mashaowei@h3c.com>
2018-02-13 11:21:08 +0800

    fdm 流水表增量统一为一个python脚本

--- python/fdm_incr.py
+++ python/fdm_incr.py
@@ -28,6 +28,32 @@ set hive.exec.compress.output=true;
 set mapred.output.compression.codec=org.apache.hadoop.io.compress.SnappyCodec; 
 set mapred.output.compression.type=block;
 
+--  DESCRIPTION: ods->fdm 一卡通交易记录表(fdm_ykt_jy)
+INSERT OVERWRITE TABLE fdm.fdm_ykt_jy PARTITION (dt = '"""+data_day_str+"""') 
+SELECT
+a.kh,
+NULL,
+b.xh,
+NULL,
+A.XFJE,
+NULL,
+A.XFRQ,
+a.xfrqsj,
+C.SHMC,
+c.shid,
+c.shmc,
+a.xfsbbh,
+NULL,
+a.xfye 
+FROM
+( SELECT kh,xfje,xfrq,xfrqsj,xfsbbh,xfye,xfdwid FROM ods.ods_usr_gxsj_t_ykt_xfjl  WHERE dt = '"""+data_day_str+"""' ) A
+LEFT JOIN ( SELECT kh,xh FROM ods.ods_usr_gxsj_t_ykt_KH WHERE dt = '2999-12-31'  ) B ON A.KH = B.kh
+LEFT JOIN ( SELECT shmc,shid,xfsbid,xfdwid FROM ods.ods_usr_gxsj_t_ykt_xfsbbh WHERE dt = '2999-12-31') c ON (
+CAST ( CAST ( C.xfsbid AS int ) AS string )) = a.xfsbbh 
+AND (
+CAST ( CAST ( c.xfdwid AS int ) AS string )) = a.xfdwid;
+
+
 --  DESCRIPTION: ods->fdm 图书借阅记录表(fdm_ts_jyjl)
 INSERT OVERWRITE TABLE fdm.fdm_ts_jyjl PARTITION (dt = '"""+data_day_str+"""') 
 SELECT
@@ -57,8 +83,26 @@ LEFT JOIN (SELECT tstm, tm FROM ods.ods_usr_gxsj_T_TS_TSXX) b ON a.TSTM = b.TSTM
 LEFT JOIN ods.ods_usr_gxsj_t_ts_dz c ON a.sfrzh = c.sfrzh;
 
 
-
-
+--  DESCRIPTION: ods->fdm 门禁记录表(fdm_bks_mj)
+INSERT OVERWRITE TABLE fdm.fdm_bks_mj PARTITION (dt = '"""+data_day_str+"""') 
+SELECT
+   a.num,
+   b.xm,
+   NULL,
+   b.bh,
+   a.inoutdate,
+   NULL,
+   (case when a.direction=='入' or a.direction=='无入'  or a.direction=='反入' then '1' else '2' end),
+   NULL,
+   NULL,a.dt FROM (
+      SELECT
+        num,
+        inoutdate,
+        direction,
+        dt
+      FROM ods.ods_usr_gxsj_t_xscrxx WHERE dt = '"""+data_day_str+"""'
+   ) a
+LEFT JOIN (SELECT xh, xm,bh FROM ods.ods_usr_gxsj_t_bzks) b ON a.num = b.xh;
 
 """
 #hiveShell = 'hive -e "' + sql + '"'
